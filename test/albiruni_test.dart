7dbf8c8b2cf9b6633d215fc51ad193bb8bb7843e
iqfareez <iqmal3@outlook.com>
2022-07-31 14:04:41 +0800

    :white_check_mark: Add tests

--- test/albiruni_test.dart
+++ test/albiruni_test.dart
@@ -3,8 +3,37 @@ import 'package:test/test.dart';
 
 void main() {
   test('Reachability', () async {
-    final awesome =
-        await Albiruni(session: "2020/2021", semester: 1).fetch("AED");
-    expect(awesome, isNotEmpty);
+    final albiruni =
+        await Albiruni(session: "2021/2022", semester: 1).fetch("AED");
+    print('Content length: ${albiruni.length}');
+    expect(albiruni, isNotEmpty);
+  });
+
+  test('Reachability proxy', () async {
+    final albiruni = await Albiruni(session: "2021/2022", semester: 1)
+        .fetch("ECONS", useProxy: true);
+    print('Content length: ${albiruni.length}');
+    expect(albiruni, isNotEmpty);
+  });
+
+  test('format input', () {
+    List<String> examples = [
+      "mcte3103",
+      "rkud4995",
+      "tqb 2011 e",
+      "tq   3001",
+      "BMCO 1105"
+    ];
+    List<String> expectedOutput = [
+      "MCTE 3103",
+      "RKUD 4995",
+      "TQB 2011E",
+      "TQ 3001",
+      "BMCO 1105"
+    ];
+
+    for (int i = 0; i < examples.length; i++) {
+      expect(examples[i].toAlbiruniFormat(), expectedOutput[i]);
+    }
   });
 }
