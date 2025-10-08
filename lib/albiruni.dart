0d856a69d5d048c87f3a60b8e841e68492f00e14
fareez <iqmal3@outlook.com>
2025-10-08 23:25:24 +0800

    🚨 remove library name because it was unnecessary

--- lib/albiruni.dart
+++ lib/albiruni.dart
@@ -1,5 +1,5 @@
 /// Albiruni library
-library albiruni;
+library;
 
 export 'src/albiruni_base.dart';
 export 'src/model/subject.dart';
@@ -7,5 +7,3 @@ export 'src/util/exceptions.dart';
 export 'src/model/day_time.dart' hide TempDayTime;
 export 'src/util/extension_methods.dart';
 export 'src/model/study_grade.dart';
-
-// TODO: Export any libraries intended for clients of this package.
